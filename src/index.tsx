198bcdcac50766290cd9f7d429ef41288acf49c5
surreptus <47543507+surreptus@users.noreply.github.com>
2021-10-24 14:45:53 -0700

    chore: setup build and firebase settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDjKUdCRvNXalqrt1IUoHvZc_AYIMwawGc",
+  authDomain: "scritch-4e385.firebaseapp.com",
+  databaseURL: "https://scritch-4e385-default-rtdb.firebaseio.com",
+  projectId: "scritch-4e385",
+  storageBucket: "scritch-4e385.appspot.com",
+  messagingSenderId: "196355494695",
+  appId: "1:196355494695:web:fe0ea5f9d9225d2888cf02",
+  measurementId: "G-VJ2Z6B6BVS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.render(
   <React.StrictMode>
