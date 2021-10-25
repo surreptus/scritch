5c14c6ae65c18273d026ac350c6f009ca954bb6d
surreptus <47543507+surreptus@users.noreply.github.com>
2021-10-24 18:05:21 -0700

    feat: add score page

--- src/pages/games/[id]/score.tsx
+++ src/pages/games/[id]/score.tsx
@@ -1,7 +1,61 @@
 import React from 'react'
+import {
+  Heading,
+  Container,
+  Table,
+  Thead,
+  Tbody,
+  Th,
+  Tr,
+  Td
+} from '@chakra-ui/react'
 
 export default function Score() {
+  const players = [
+    {
+      name: 'tony', 
+      score: 10,
+      id: 1
+    },
+    {
+      name: 'john', 
+      score: 210,
+      id: 22 
+    },
+  ]
+
   return (
-    <div> hello score</div>
+    <Container>
+      <Heading>
+        Scores
+      </Heading>
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>Place</Th>
+            <Th>Name</Th>
+            <Th isNumeric>Score</Th>
+          </Tr>
+        </Thead>
+
+        <Tbody>
+          {players.map((player, index) => (
+            <Tr>
+              <Td>
+                {index}
+              </Td>
+
+              <Td>
+                {player.name}
+              </Td>
+
+              <Td isNumeric>
+                {player.score}
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </Container>
   )
 }
